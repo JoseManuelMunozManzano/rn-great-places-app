7824600c9223e1a85900c77255e5b1cf96ecfa40
Neimerc <Neil_Mercury@yahoo.es>
2021-04-08 17:41:32 +0200

    Showing a Map Preview of the Location

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -11,6 +11,7 @@ import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
 import Colors from '../constants/Colors';
+import MapPreview from './MapPreview';
 
 const LocationPicker = () => {
   const [isFetching, setIsFetching] = useState(false);
@@ -41,7 +42,7 @@ const LocationPicker = () => {
       const location = await Location.getCurrentPositionAsync({
         timeout: 5000,
       });
-      console.log(location);
+      // console.log(location);
       setPickedLocation({
         lat: location.coords.latitude,
         lng: location.coords.longitude,
@@ -58,13 +59,13 @@ const LocationPicker = () => {
 
   return (
     <View style={styles.locationPicker}>
-      <View style={styles.mapPreview}>
+      <MapPreview style={styles.mapPreview} location={pickedLocation}>
         {isFetching ? (
           <ActivityIndicator size="large" color={Colors.primary} />
         ) : (
           <Text>No location chosen yet!</Text>
         )}
-      </View>
+      </MapPreview>
       <Button
         title="Get User Location"
         color={Colors.primary}
@@ -84,8 +85,6 @@ const styles = StyleSheet.create({
     height: 150,
     borderColor: '#ccc',
     borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
