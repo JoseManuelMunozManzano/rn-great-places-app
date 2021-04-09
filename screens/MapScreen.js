b67405f42058f33a9085a9d737180bf9fff43031
Neimerc <Neil_Mercury@yahoo.es>
2021-04-09 10:29:56 +0200

    Finishing the Map Screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -5,16 +5,23 @@ import MapView, { Marker } from 'react-native-maps';
 import Colors from '../constants/Colors';
 
 const MapScreen = ({ navigation }) => {
-  const [selectedLocation, setSelectedLocation] = useState();
+  const initialLocation = navigation.getParam('initialLocation');
+  const readonly = navigation.getParam('readonly');
+
+  const [selectedLocation, setSelectedLocation] = useState(initialLocation);
 
   const mapRegion = {
-    latitude: 37.78,
-    longitude: -122.43,
+    latitude: initialLocation ? initialLocation.lat : 37.78,
+    longitude: initialLocation ? initialLocation.lng : -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
 
   const selectLocationHandler = event => {
+    if (readonly) {
+      return;
+    }
+
     setSelectedLocation({
       lat: event.nativeEvent.coordinate.latitude,
       lng: event.nativeEvent.coordinate.longitude,
@@ -57,6 +64,11 @@ const MapScreen = ({ navigation }) => {
 
 MapScreen.navigationOptions = navData => {
   const saveFn = navData.navigation.getParam('saveLocation');
+  const readonly = navData.navigation.getParam('readonly');
+
+  if (readonly) {
+    return {};
+  }
 
   return {
     headerRight: () => (
--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -11,6 +11,15 @@ const PlaceDetailScreen = ({ navigation }) => {
     state.places.places.find(place => place.id === placeId)
   );
 
+  const selectedLocation = { lat: selectedPlace.lat, lng: selectedPlace.lng };
+
+  const showMapHandler = () => {
+    navigation.navigate('Map', {
+      readonly: true,
+      initialLocation: selectedLocation,
+    });
+  };
+
   return (
     <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
       <Image style={styles.image} source={{ uri: selectedPlace.imageUri }} />
@@ -20,7 +29,8 @@ const PlaceDetailScreen = ({ navigation }) => {
         </View>
         <MapPreview
           style={styles.mapPreview}
-          location={{ lat: selectedPlace.lat, lng: selectedPlace.lng }}
+          location={selectedLocation}
+          onPress={showMapHandler}
         />
       </View>
     </ScrollView>
